163de6e2602c666799667a1be52bf3b0f51559c3
Bhupendra-Verma1 <bhupendrav945@gmail.com>
2025-12-19 08:21:56 +0530

    Improving design issue on home page

--- src/components/landing/HeroSection.jsx
+++ src/components/landing/HeroSection.jsx
@@ -1,9 +1,9 @@
 import assets from '../../assets/assets'
-const HeroSection = ({openSignIn, openSignUp}) => {
+const HeroSection = ({ openSignIn, openSignUp }) => {
     return (
         <div className="landing-page-content relative">
-            <div className="absolute inset-0 bg-linear-to-r from-purple-50 to-indigo-50 opacity-80 z-0 pointer-events-none"> 
-                
+            <div className="absolute inset-0 bg-linear-to-r from-purple-50 to-indigo-50 opacity-80 z-0 pointer-events-none">
+
             </div>
 
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
@@ -18,16 +18,17 @@ const HeroSection = ({openSignIn, openSignUp}) => {
                         </p>
 
                         <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
-                            <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
+                            <div className="space-y-0 inline-grid grid-cols-2 gap-5 mx-auto">
                                 <button
                                     onClick={() => openSignUp()}
                                     className="flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purple-500 hover:bg-purple-600 md:py-4 md:text-lg md:px-10 transition-all duration-200 shadow-lg hover:shadow-xl"
                                 >
                                     Get Started
                                 </button>
-                                <button 
-                                    onClick = {() => openSignIn()}
-                                    className="flex items-center justify-center px-6 py-3 border border-gray-300 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 md-py-4 md:text-lg md:px-10 transition-all duration-300 shadow-md hover:shadow-lg"
+
+                                <button
+                                    onClick={() => openSignIn()}
+                                    className="flex items-center justify-center px-6 py-3 border border-gray-300 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10 transition-all duration-300 shadow-md hover:shadow-lg"
                                 >
                                     Sign In
                                 </button>
