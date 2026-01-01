df6292a86c4e6a51083ab0411486b6cf5c086295
Bhupendra-Verma1 <bhupendrav945@gmail.com>
2026-01-01 10:40:05 +0530

    Update the ApiEndpoint

--- src/util/ApiEndpoint.js
+++ src/util/ApiEndpoint.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://cloudshareapi-n9zo.onrender.com";
+const BASE_URL = "https://cloudshareapi-n9zo.onrender.com/api/v1.0";
 // const BASE_URL = "http://localhost:8080/api/v1.0";
 
 export const apiEndpoints = {
